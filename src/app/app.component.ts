e77bc0a6f847e29b3aacb0f35be20cd7e3e69f0c
Rahul Rao <RAOR378647@exactsoftware.com>
2020-08-02 19:48:17 +0530

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormGroup, FormControl, FormArray, FormBuilder } from '@angular/forms'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,37 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'angular-form-array-app';
+  skillsForm: FormGroup;
+  constructor(private fb: FormBuilder) {
+
+    this.skillsForm = this.fb.group({
+      name: '',
+      skills: this.fb.array([]),
+    });
+
+  }
+
+  //getter
+  get skills(): FormArray {
+    return this.skillsForm.get("skills") as FormArray
+  }
+
+  newSkill(): FormGroup {
+    return this.fb.group({
+      skill: '',
+      exp: '',
+    })
+  }
+
+  addSkills() {
+    this.skills.push(this.newSkill());
+  }
+
+  removeSkill(i: number) {
+    this.skills.removeAt(i);
+  }
+
+  onSubmit() {
+    console.log(this.skillsForm.value);
+  }
 }
